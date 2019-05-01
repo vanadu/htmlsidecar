df632e416622d6fa2239f1a93573ca8b44e596d1
unknown <van@vanocular.com>
2019-05-01 07:40:12 -0400

    FileReader object loaded

--- src/js/scripts_v2.js
+++ src/js/scripts_v2.js
@@ -116,6 +116,32 @@ var Dimwhit = (function () {
       appcontainer: document.querySelector(dynamicRegions.appContainer)
     };
 
+    function getCurIasdasfdmg(curImg, fileName) {
+      var a, b;
+      a = curImg;
+      b = fileName;
+      console.log('a is: ' + a);
+      console.log('b is: ' + b);
+      console.log('Appobj.currentimg is...');
+      console.log(Appobj.currentimg);
+      document.querySelector(staticRegions.dropArea).style.display = 'none';
+      const curImgDiv = document.createElement('div');
+    
+      // Assign an id to the new div -- we're not adding this to an object because it's created here and is only used here. NOTE: These containers divs don't appear in devMode when the curImg is accessed from the index.html 
+      curImgDiv.id = 'main-img-container';
+      // Insert main-img-container into the existing main-image
+      document.getElementById('main-image').insertBefore(curImgDiv, null);
+      // !VA insert main-img-container into the newly-created main-img-container
+      document.getElementById('main-img-container').insertBefore(dynamicRegions.curImg, null);
+      // Create the image object and read in the binary image from the FileReader object.
+      // This allows access of image properties. You can't get image properties from a FileReader object -- it's just a blob' 
+
+    }
+
+    // function handleFileSelect () {
+    //   console.log('HandleFIleSelect running..');
+    // }
+    
     // !VA Functions that get returned from the UIContoller object go here
     return {
       // !VA V2 Return all the strings for the UI element's IDs
@@ -141,7 +167,101 @@ var Dimwhit = (function () {
         return ccpBuildTag;
       },
 
+      //FILEREADER OBJECT PROCESSING
+      //Get the user-selected image file object 
+      handleFileSelect: function(evt) {
+        console.log('RUnning HandleFileSelect');
+        // If a file is already being displayed, i.e. appObj.filename is true, then remove that image to make room for the next image being dropped
+        if (Appobj.currentimg) {
+          // !VA 03.18.18 Don't need the var declaration anymore - it's in the appRegions object
+          // var curImg = document.getElementById('main-img');
+          dynamicRegions.curImg.parentNode.removeChild(dynamicRegions.curImg);
+        }
+        // console.log('HANDLEFILESELECT');
+        //The drop event has been executed and handleFileSelect is running.
+        // !VA Can't remember what this does...    
+        evt.stopPropagation();
+        evt.preventDefault();
+        //dataTransfer object is used to hold object data during a drag operation
+        var files = evt.dataTransfer.files; // FileList object.
+        // files is a FileList of File objects. List some properties.
+        //Note that the File objects are blob objects that include the parameter
+        // type, which indicates the type of file. 
+        // console.log('files is: ' + files);
+        // !VA I don't think the output array is used here, so commenting out.
+        // var output = [];
+        // !VA get the number of files selected
+        // console.log('files.length is:' + files.length);
+        
+        var f =  files[0];
+        // console.log('f.name is: ' + f.name);
+        // !VA this for loop would be used if we were using the entire filelist instead of just one
+        // a single dropped file
+        // for (var i = 0, f; f = files[i]; i++) {
+        // console.log('files[i].name is: ' + files[i].name);
+        // Only process image files.
+        //This is the query for file type -- it includes any MIME type that starts 
+        //with 'image' which is a huge list of possible formats -- see the complete
+        //list of MIME formats. Best to narrow that down to JPG, GIF, PNG -- not sure
+        //about SVG and VML, they're not in the big list of MIME types I found 
+        // !VA Need to handle this error
+        if (!f.type.match('image.*')) {
+          console.log('NOT IMAGE');
+          var target = "notimage";
+          // console.log('target is: ' + target);
+          // !VA Below is the error handler - skipping for now
+          // var isErr = errorHandler(target, 0, 0);
+          console.log('!VA Not an image file error: Exiting...');
+          return;
+        }
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+          return function(e) {
+            // Create the image object        
+            var curImg = new Image();
+            // !VA create the id
+            curImg.id = 'main-img';
+            // !VA assign the blob in e.target.result to the source of the image
+            // curImg.src = e.target.result;
+            curImg.src = e.target.result;
+            let fileName;
+            // Read the filename of the FileReader object into a variable to pass to the getCurImage function, otherwise the blob has no name
+            fileName = theFile.name;
+            // Pass the FileObject i.e. new Image object to the function that gets the image data
+            // !VA  03.18.18 Put the curImg created with FileReader in the appRegions object
+            dynamicRegions.curImg = curImg;
+            console.log('Showing curImg...');
+            console.dir(curImg);
+            Appobj.currentimg = curImg;
+            Appobj.filename =fileName;
+            console.dir(Appobj);
 
+            // !VA Trying to understand how Dimwhit 1 handles the image display
+            console.log(Appobj.currentimg);
+            document.querySelector(staticRegions.dropArea).style.display = 'none';
+            const curImgDiv = document.createElement('div');
+          
+            // Assign an id to the new div -- we're not adding this to an object because it's created here and is only used here. NOTE: These containers divs don't appear in devMode when the curImg is accessed from the index.html 
+            curImgDiv.id = 'main-img-container';
+            // Insert main-img-container into the existing main-image
+            document.getElementById('main-image').insertBefore(curImgDiv, null);
+            // !VA insert main-img-container into the newly-created main-img-container
+            document.getElementById('main-img-container').insertBefore(dynamicRegions.curImg, null);
+            // Create the image object and read in the binary image from the FileReader object.
+            // This allows access of image properties. You can't get image properties from a FileReader object -- it's just a blob' 
+
+            
+
+          };
+        })(f);
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
+        // }
+      },
+
+
+      //FILEREADER OBJECT PROCESSING END
 
 
       // !VA Populate Appdata using the properties of the dynamic regions in the 
@@ -291,10 +411,40 @@ var Dimwhit = (function () {
     var dimViewers = UIController.getDimViewerIDs();
     // var Appobj = UIController.getAppobj();
 
- 
+
+
+
     var setupEventListeners = function() {
-      
+
+      //DRAG AND DROP PROCESSING START
+      //Drag and Drop Handler 
+      function handleDragOver(evt) {
+        //prevent the bubbling of the event to the parent event handler
+        evt.stopPropagation();
+        //prevent the default action of the element from executing -- so in this case
+        //I think since it is a div the default event would be for some browsers to 
+        //open the file in the browser when dropped
+        evt.preventDefault();
+        //console.log('dragging over');
+        evt.dataTransfer.dropEffect = 'copy'; // Explicitly show this is a copy.
+        console.log('Calling HandleFileSelect');
+      }
+
+      // Event Listeners for Drag and Drop
+      // !VA dropZone is the screen region that will accept the drop event 
+      var dropZone = document.querySelector(dynamicRegions.appContainer);
+      dropZone.addEventListener('dragover', handleDragOver, false);
+    
+      dropZone.addEventListener('drop', UIController.handleFileSelect, false);
+      // dropZone.addEventListener('drop', startNewDrop, false);
+      // Drag and Drop Listener 
+
+      //DRAG AND DROP PROCESSING END
+
+
     };
+
+
 
 
     var initializeDOM = function() {
@@ -322,6 +472,7 @@ var Dimwhit = (function () {
 
 
       } else {
+
         // !VA This is USER MODE -- there's no hardcoded image in the HTML file. 
         console.log('USER MODE: no current image');
         // !VA V2 Show the dropArea
