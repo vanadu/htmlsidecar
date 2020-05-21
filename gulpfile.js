6c2f12039d1a1f2c06ff9ac96170292b48162df0
unknown <van@vanocular.com>
2020-05-21 10:50:52 -0400

    Upgraded to gulp 4.0.2

--- gulpfile.js
+++ gulpfile.js
@@ -1,216 +1,170 @@
-// Gulp.js configuration
-// Commenting out everything except dev operations
-var
-// modules
-gulp = require('gulp'),
-  // plugins = require('gulp-load-plugins'),
-  // newer = require('gulp-newer'),
-  // newer = require('gulp-newer'),
-  // imagemin = require('gulp-imagemin'),
-  // htmlclean = require('gulp-htmlclean'),
-  // concat = require('gulp-concat'),
-  // deporder = require('gulp-deporder'),
-  // stripdebug = require('gulp-strip-debug'),
-  // uglify = require('gulp-uglify'),
+// !VA Gulpfile creation based on Craig Buckler: https://www.sitepoint.com/introduction-gulp-js/ with browser-sync tips from https://medium.com/swlh/setting-up-gulp-4-0-2-for-bootstrap-sass-and-browsersync-7917f5f5d2c5
+
+// !VA NOTE: Buckler makes no reference to gulp requiring a default task, but gulp 4 requires a default task to run. While working on this before I got to the part where Buckley adds the default task, I added a dummy 'hello' default task 'logging' below just to write to the console, it's still in there just commented out. It shows how to write to the console in gulp.
+// !VA NOTE: Installing gulp-noop appears to have uninstalled all the other gulp modules used here, including browser-sync. Had to reinstall a number of modules.
+// !VA NOTE: THe most frequent mistake I make is including a comma after the last const declaration in the list.
+// !VA NOTE: Sourcemaps aren't located in a separate map file, they're integrated into the CSS output now but they appear to work as long as the CSS output is piped to the src folder.
+const 
+  gulp = require('gulp'),
+  newer = require('gulp-newer'),
+  imagemin = require('gulp-imagemin'),
+  htmlclean = require('gulp-htmlclean'),
+  noop = require('gulp-noop'),
+  concat = require('gulp-concat'),
+  deporder = require('gulp-deporder'),
+  terser = require('gulp-terser'),
+  stripdebug = require('gulp-strip-debug'),
+  sourcemaps = require('gulp-sourcemaps'),
   sass = require('gulp-sass'),
-  // postcss = require('gulp-postcss'),
-  // assets = require('postcss-assets'),
-  // autoprefixer = require('autoprefixer'),
-  // mqpacker = require('css-mqpacker'),
-  // cssnano = require('cssnano'),
-  sourcemaps = require('gulp-sourcemaps');
-  browserSync = require('browser-sync').create();
+  postcss = require('gulp-postcss'),
+  assets = require('postcss-assets'),
+  autoprefixer = require('autoprefixer'),
+  mqpacker = require('css-mqpacker'),
+  cssnano = require('cssnano')
+  browserSync = require('browser-sync').create()
+  ;
+
 
 // development mode?
-  devBuild = (process.env.NODE_ENV !== 'production'),
+// !VA This depends on a NODE.ENV variable that is not set on my machine. Can't comment out this line because it's referenced below. If I comment it out, I need to remove all references to it.
+devBuild = (process.env.NODE_ENV !== 'production'),
 
-// folders
-folder = {
-  src: 'app/',
-  build: 'dist/'
-};
-// image processing - newer/imagenim
-gulp.task('images', function() {
-  var out = folder.build + 'img/';
-  return gulp.src(folder.src + 'img/**/*')
+// NOTE: Buckler creates folder path variables directly, not in an object. Other gulpfiles I've seen use:
+// folder = {
+//   src: 'app/',
+//   build: 'dist/'
+// };
+// which requires referencing the path as folder.app or folder.src. Buckley just references the folders with variables:
+src = 'app/',
+build = 'dist/'
+
+// !VA Use optimize images and output to /dist
+function images() {
+  const out = build + 'img/';
+  return gulp.src(src + 'img/**/*')
     .pipe(newer(out))
     .pipe(imagemin({ optimizationLevel: 5 }))
     .pipe(gulp.dest(out));
-});
-// HTML processing - htmlclean
-gulp.task('html', ['images'], function() {
-  var
-    //out = folder.build + 'html/', :VA
-    out = folder.build + '/',
-    // page = gulp.src(folder.src + 'html/**/*') :VA
-    page = gulp.src(folder.src + '/**/*')
-      .pipe(newer(out));
+  // !VA }); Buckler includes extra closing para here that breaks code. Why the fuck?
+};
 
-  // minify production code
-  if (!devBuild) {
-    page = page.pipe(htmlclean());
-  }
+// HTML processing -- Buckler says this only runs if NODE_ENV is set to 'production'. That would be specified in the ternary below, with noop being run if devBuild is set. But I deleted that development mode line above and ran it again, and still no HTML file was processed. I think that means that the NODE_ENV variable is not set. I don't know how to do that yet. Leave everything as is for now so it is ignored, see below on how to run htmlclean.
+// !VA Changed the htmlclean line to: .pipe(htmlclean()) and copied the index.html file to app/html/index.html. It worked, but it requires putting the HTML file in a separate folder, so I will probably not use that, since I see no reason to obfuscate my HTML file.
+function html() {
+  const out = build + 'html/';
+  return gulp.src(src + 'html/**/*')
+    .pipe(newer(out))  //; !VA Buckler includes semicolon here that break code. Why the fuck? Second time.
+    .pipe(devBuild ? noop() : htmlclean())
+    // .pipe(htmlclean())
+    .pipe(gulp.dest(out));
+}
 
-  return page.pipe(gulp.dest(out));
-});
-// JavaScript processing - deporder, concat, strip-debug, uglify
-gulp.task('js', function() {
-  
-  var jsbuild = gulp.src(folder.src + 'js/**/*')
+// JavaScript processing. 
+// !VA Runs as expected.
+// !VA Need to include the obfuscate plugin here
+function js() {
+  return gulp.src(src + 'js/**/*')
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
     .pipe(deporder())
-    // Changed :VA
-    .pipe(concat('scripts.js'));
+    // !VA Changed output file from main.js to app.js
+    .pipe(concat('app.js'))
+    .pipe(stripdebug ? stripdebug() : noop())
+    .pipe(terser())
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    .pipe(gulp.dest(build + 'js/'));
+}
 
-  if (!devBuild) {
-    jsbuild = jsbuild
-      .pipe(stripdebug())
-      .pipe(uglify());
-  }
-  
-  return jsbuild.pipe(gulp.dest(folder.build + 'js/'));
-  
-});
-// CSS processing -- sass, postcss, postcss-assets, autoprefixer, cssmqpacker, cssnano
-gulp.task('css', ['images'], function() {
-  
-  var postCssOpts = [
-    assets({ loadPaths: ['img/'] }),
-    autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
-    mqpacker
-  ];
-
-  if (!devBuild) {
-    postCssOpts.push(cssnano);
-  }
-
-  return gulp.src(folder.src + 'scss/style.scss')
+// CSS processing
+// !VA Had to reinstall gulp-sass because it magically hd been deleted. Won't let me install it for some reason. The reason was that a hidden bash was already running, all I had to do was close it in Windows Task Manager. After that, ran fine.
+function css() {
+  // !VA Changing main.scss to style.scss, since that's the one that loads all partials
+  // return gulp.src(src + 'scss/main.scss')
+  return gulp.src(src + 'scss/style.scss')
+    .pipe(sourcemaps ? sourcemaps.init() : noop())
     .pipe(sass({
       outputStyle: 'nested',
-      imagePath: 'img/',
+      imagePath: '/images/',
       precision: 3,
       errLogToConsole: true
-    }))
-    .pipe(postcss(postCssOpts))
-    .pipe(gulp.dest(folder.build + 'css/'));
+    }).on('error', sass.logError))
+    .pipe(postcss([
+      assets({ loadPaths: ['images/'] }),
+      // !VA This is fix #1 from the stupid browserlist error message issue: autoprefixer({ browsers: ['last 2 versions', '> 2%'] }),
+      autoprefixer(),
+      mqpacker,
+      cssnano
+    ]))
+    .pipe(sourcemaps ? sourcemaps.write() : noop())
+    // !VA The CSS file need to go to app, not build, during dev. So we can pipe to two separate locations, added the one to src. 
+    .pipe(gulp.dest(src + 'css/'))
+    .pipe(browserSync.stream())
+    .pipe(gulp.dest(build + 'css/'));
+}
 
-});
-// run all tasks
-// gulp.task('run', ['html', 'css', 'js']);
-// watch for changes -- only applies to production operations, so add
-// to task list when prepping for production
-gulp.task('watch', function() {
-  
-  // image changes -- commented out for dev :VA
-  // gulp.watch(folder.src + 'img/**/*', ['images']);
+// !VA Now that we have the default task below, we can delete the dummy 'hello' task I had to create to get gulp to run at all.
+// function hello(cb) {
+//   console.log('Hi Van');
+//   console.log('build is: ' + build);
+//   cb();
+// }
+// gulp.task('default', gulp.series(hello))
 
-  // html changes -- commented out for dev :VA
-  // gulp.watch(folder.src + '/**/*', ['html']);
+// !VA Expose the js function
+exports.js = js;
+// !VA Expose the html and images functions and run them in series
+exports.html = gulp.series(html, images);
+// !VA Expose the images and css functions and run them in series. However, we are running the css function now from the browsersync serve function, so we don't need to do it here.
+// exports.css = gulp.series(images, css);
+exports.css = gulp.series(images);
+// Export all the tasks to be run for the production build now. Note that we've removed the css function from this list because it's run now in the browser-sync serve function.
+// exports.build = gulp.parallel(exports.html, exports.css, exports.js)
+exports.build = gulp.parallel(exports.html, exports.js)
 
-  // javascript changes - commented out for dev :VA
-  // gulp.watch(folder.src + 'js/**/*', ['js']);
+// !VA We don't want to watch for file changes to images at all, and changes to html and jhavascript are done in the browser-sync serve function. So we are deleting this watch function. When we want to compress images or minify the JS we will run those tasks manually.
+// // watch for file changes
+// function watch(done) {
+//   // image changes
+//   gulp.watch(src + 'images/**/*', images);
+//   // html changes
+//   gulp.watch(src + 'html/**/*', html);
+//   // css changes
+//   gulp.watch(src + 'scss/**/*', css);
+//   // js changes
+//   gulp.watch(src + 'js/**/*', js);
+//   done();
+// }
+// exports.watch = watch;
 
-  // css changes -- not necessary because this is in the browser-sync routine now :VA
-  // gulp.watch(folder.src + 'scss/**/*', ['css']);
-  
-});
-
-// There are old sourcemap scripts I couldn't get to work when I was
-// originally just trying to get something to work -- revisit later
-
-// From https://blog.yipl.com.np/using-gulp-browser-sync-and-source-maps-ef14e0903982
-/*
- * Compile files from _scss
- */
-// gulp.task('sass', function () {
-//   return gulp.src('scss/main.scss')
-//       .pipe(sourcemaps.init())
-//           .pipe(sass({
-//               includePaths: ['css'],
-//               onError: browserSync.notify
-//           }))
-//           .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-//           .pipe(gulp.dest('css'))
-//           .pipe(browserSync.reload({stream:true}))
-//       .pipe(sourcemaps.write('../maps'))
-//       .pipe(gulp.dest('css'));
-// });
-
-// From https://gist.github.com/marceloogeda/5a449caa50462ab2667540a93d34009f
-// gulp.task('styles', () => {
-//   return gulp.src('scss/**/*.scss')
-//       .pipe(plugins().sourcemaps.init())
-//       .pipe(plugins().sass().on('error', plugins().sass.logError))
-//       .pipe(plugins().sourcemaps.write('../maps'))
-//       .pipe(gulp.dest('css'))
-//       .pipe(browserSync.stream());
-// });
-
-// Don't know where this is from but works for me now
-gulp.task('map', function() {
-  gulp.src('app/scss/style.scss')
-  // .pipe(sourcemaps.init())
-    .pipe(sass())
-    .on('error', function (err) {
-      console.log(err.toString());
-
-      this.emit('end');
-    })
-    // .pipe(sourcemaps.write('.'))
-    // had a hell of a time figuring out the pattern for writing files
-    .pipe(gulp.dest('app/css'));
-});
-
-
-// Static Server + watching scss/html files
-gulp.task('serve', ['sass'], function() {
-  
+function serve() {
   browserSync.init({
-    server: "./app",
-    port: 3300,
-    // Port for the browser-sync UI
-    ui: {
-      port: 3301
-    }
-    // Tried to use reload delay but the main img still doesn't load 
-    // reloadDelay: 3000
-    // Might need this... :VA
-    // server: {
-    //   baseDir: "public" // Change this to your web root dir
-    // }
+      server: {
+         baseDir: src,
+         index: "/index.html",
+         server: "./app",
+      },
+      port: 3300,
+      // Port for the browser-sync UI
+      ui: {
+        port: 3301
+      }
   });
-  
-        
-  // gulp.watch("app/scss/*.scss", ['sass']);
-  gulp.watch("app/scss/**/*.scss", ['sass']);
+  // !VA watch is a predefined gulp function, not to be confused with a user-sefined watch function as below. The watch function takes a second argument that specifies the function that contains the browsersync stream to include in the watch method. If you don't include that argument, browsersync won't update that stream. So include the 'css' function as artument to the watch method run on the scss files. Also, I'm not sure whether we need to include all the scss or just style.scss since that's the one that includes all the partials.
+  gulp.watch(src + 'scss/**/*.scss', css);
+  gulp.watch(src + '*.html').on('change',browserSync.reload);
+  gulp.watch(src + 'js/**/*.js').on('change', browserSync.reload);
+}
+// !VA Expose the serve function
+exports.serve = serve;
 
-  
+// !VA Getting this message: Replace Autoprefixer browsers option to Browserslist config. It appears to be a notification rather than an error message, but it appears every time SCSS is updated. Super annoying. Looked here: https://github.com/gatsbyjs/gatsby/issues/14530 and the fix is: 1) Remove the 'last two versions' statement from the postcss pipe above and replace with just the call without the parameter 2) Add this to the package.json file
 
-  //enclose multiple file parameters in brackets -- works :VA
-  gulp.watch(['app/*.html', 'app/js/*.js']).on('change', browserSync.reload);
-  // gulp.watch("").on('change', browserSync.reload); don't need anymore, see above :VA
-});
+// "browserslist": [
+//   "last 2 version",
+//   "> 2%"
+// ],
 
-// Compile sass into CSS & auto-inject into browsers
-gulp.task('sass', function() {
-  return gulp.src("app/scss/style.scss")
-    .pipe(sourcemaps.init())
-    .pipe(sass())
-    // Prevent gulp from abort on error every time a property is replaced with a variable
-    // hence preventing an 'undeclared variable' error. Not the most elegant of solutions
-    // because it writes an ugly error to the console ever time but it works for now. :VA
-    .on('error', function (err) {
-      console.log(err.toString());
-      this.emit('end');
-    })
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest("app/css"))
-    .pipe(browserSync.stream());
-});
-  
-// default task
-// gulp.task('default', ['run', 'maps', 'watch', 'serve']);
-// gulp.task('default', ['map', 'serve']);
-gulp.task('default', ['serve']);
-  
-  
+// !VA Then the message goes away.
+
+// !VA This is the default task before implementing browser-sync, but we don't need to do all this until production. Until then all we need to do is compile the CSS and watch for changes. For production we can run the 'js' and 'html' functions manually with 'gulp build'.
+gulp.task('default', gulp.series(exports.serve));
 
